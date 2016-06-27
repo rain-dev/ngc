461c287f16c28b64f85b578b0a9b6675bccaa5ec
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-06-27 18:09:11 +0200

    Added constexpr if showcase.

---
+++ reference/if/showcase/constexpr.cpp
@@ -0,0 +1,35 @@
+/* Global scope forward injection point */
+
+struct __ngc_0_conditional_true__;
+struct __ngc_0_conditional_false__;
+
+/* Code */
+
+static constexpr bool value = true;
+
+/* Pre-function f signature and implementation injection point */
+
+struct __ngc_0_conditional_true__
+{
+  static inline void execute()
+  {
+    constexpr int k = 17;
+    std :: cout << k << std :: endl;
+  }
+};
+
+struct __ngc_0_conditional_false__
+{
+  static inline void execute()
+  {
+    std :: cout << "Nothing to say." << std :: endl;
+  }
+};
+
+/* Code */
+
+void f()
+{
+  constexpr int k = 17;
+  __ngc_conditional__ <value, __ngc_0_conditional_true__, __ngc_0_conditional_false__> :: type :: execute();
+}
