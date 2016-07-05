bd4ef81fd7de4c39df8edcc8ff5933f15bd72405
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-07-05 19:04:54 +0200

    Added __ngc_parameter_pack__.

---
+++ lib/__ngc_parameter_pack__.h
@@ -0,0 +1,6 @@
+#ifndef __lib____ngc_parameter_pack____h
+#define __lib____ngc_parameter_pack____h
+
+template <typename... types> struct __ngc_parameter_pack__;
+
+#endif
--- lib/ngc.h
+++ lib/ngc.h
@@ -1,14 +1,16 @@
 #ifndef __lib__ngc__h
 #define __lib__ngc__h
 
+#include "__ngc_parameter_pack__.h"
+
+#include "optional/__ngc_null__.h"
+
 #include "optional/__ngc_factory__/__ngc_array_traits__.h"
 #include "optional/__ngc_factory__/__ngc_constructor__.h"
 #include "optional/__ngc_factory__/__ngc_factory__.h"
 
 #include "optional/__ngc_phantom__/__ngc_embody__.h"
 #include "optional/__ngc_phantom__/__ngc_phantom_base__.h"
-
-#include "optional/__ngc_null__.h"
 
 #include "string/string.h"
 
