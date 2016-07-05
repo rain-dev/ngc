4b6d78c2962084e76b2c3b7cd35a3828f16a19ad
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-07-05 18:43:33 +0200

    Added ngc.h as entry point for the library.

---
+++ lib/ngc.h
@@ -0,0 +1,13 @@
+#ifndef __lib__ngc__h
+#define __lib__ngc__h
+
+#include "optional/__ngc_factory__/__ngc_array_traits__.h"
+#include "optional/__ngc_factory__/__ngc_constructor__.h"
+#include "optional/__ngc_factory__/__ngc_factory__.h"
+
+#include "optional/__ngc_phantom__/__ngc_embody__.h"
+#include "optional/__ngc_phantom__/__ngc_phantom_base__.h"
+
+#include "optional/__ngc_null__.h"
+
+#endif
