1f5a8f4eb0c9d06dfaf0384a6faf757b60b4e064
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-07-20 16:34:09 +0200

    Added copyright to __ngc_type_probe__.h. Please document.

--- lib/optional/__ngc_factory__/__ngc_type_probe__.h
+++ lib/optional/__ngc_factory__/__ngc_type_probe__.h
@@ -1,3 +1,15 @@
+/**
+  Angular C core library - Rain
+
+  Released under GNU GENERAL PUBLIC LICENSE
+
+  See also:
+  - https://rain.vg
+  - https://github.com/rainvg/ngc
+
+  ------------------------------------------
+*/
+
 #ifndef __lib__optional____ngc_factory______ngc_type_probe____h
 #define __lib__optional____ngc_factory______ngc_type_probe____h
 
