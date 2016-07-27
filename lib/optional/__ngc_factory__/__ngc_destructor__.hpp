ed47c94f62b4022ff522f05c090f7490f469d4db
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-07-27 17:16:23 +0200

    Several fixes. __ngc_destruct__ works fine.

--- lib/optional/__ngc_factory__/__ngc_destructor__.hpp
+++ lib/optional/__ngc_factory__/__ngc_destructor__.hpp
@@ -17,15 +17,19 @@ template <typename type> inline void __ngc_destructor__ <false, false> :: execut
 {
 }
 
+template <typename type> inline void __ngc_destructor__ <false, true> :: null_iterator :: execute(type & that)
+{
+}
+
 template <bool dummy> template <typename type> inline void __ngc_destructor__ <false, true> :: member_iterator <0, dummy> :: execute(type & that)
 {
-  __ngc_destruct__(type :: template __ngc_member__ <0> :: get(that));
+  __ngc_destruct__(type :: template __ngc_member__ <0, false> :: get(that));
 }
 
 template <size_t index, bool dummy> template <typename type> inline void __ngc_destructor__ <false, true> :: member_iterator <index, dummy> :: execute(type & that)
 {
   member_iterator <index - 1, false> :: execute(that);
-  __ngc_destruct__(type :: template __ngc_member__ <index> :: get(that));
+  __ngc_destruct__(type :: template __ngc_member__ <index, false> :: get(that));
 }
 
 template <bool dummy> template <typename type> inline void __ngc_destructor__ <false, true> :: base_iterator <0, dummy> :: execute(type & that)
@@ -36,15 +40,15 @@ template <bool dummy> template <typename type> inline void __ngc_destructor__ <f
 template <size_t index, bool dummy> template <typename type> inline void __ngc_destructor__ <false, true> :: base_iterator <index, dummy> :: execute(type & that)
 {
   base_iterator <index - 1, false> :: execute(that);
-  __ngc_destruct__((typename type :: template __ngc_base <index, false> :: type &) that);
+  __ngc_destruct__((typename type :: template __ngc_base__ <index, false> :: type &) that);
 }
 
 template <typename type> inline void __ngc_destructor__ <false, true> :: execute(type & that)
 {
   that.__ngc_destruct__();
-  
-  std :: conditional <__ngc_member_count__ <type> :: value, member_iterator <__ngc_member_count__ <type> :: value - 1, false>, null_iterator> :: type :: execute(that);
-  std :: conditional <__ngc_base_count__ <type> :: value, base_iterator <__ngc_base_count__ <type> :: value - 1, false>, null_iterator> :: type :: execute(that);
+
+  std :: conditional <__ngc_member_count__ <type> :: value != 0, member_iterator <__ngc_member_count__ <type> :: value - 1, false>, null_iterator> :: type :: execute(that);
+  std :: conditional <__ngc_base_count__ <type> :: value != 0, base_iterator <__ngc_base_count__ <type> :: value - 1, false>, null_iterator> :: type :: execute(that);
 }
 
 template <bool is_class> template <bool dummy> template <typename type> inline void __ngc_destructor__ <true, is_class> :: iterator <0, dummy> :: execute(type & that)
